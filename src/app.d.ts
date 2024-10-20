707a6971b2b4a84082769a07c4654c081723eeaf
Joshua Tahanlangit <contact@thisjt.me>
2024-10-20 15:27:37 +0800

    feat: install viewtransition types

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,12 +2,23 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-        interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
-        }
-    }
+		interface Platform {
+			env: Env;
+			cf: CfProperties;
+			ctx: ExecutionContext;
+		}
+	}
+
+	interface ViewTransition {
+		updateCallbackDone: Promise<void>;
+		ready: Promise<void>;
+		finished: Promise<void>;
+		skipTransition: () => void;
+	}
+
+	interface Document {
+		startViewTransition(updateCallback: () => Promise<void>): ViewTransition;
+	}
 }
 
 export {};
